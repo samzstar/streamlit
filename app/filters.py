b5146dbadcf9c84ce4637c4e25c075a0215ec6c3
SamihaSyeda <samihasyeda@gmail.com>
2025-05-20 14:34:57 +0100

    added player tags filter

--- app/filters.py
+++ app/filters.py
@@ -9,8 +9,8 @@ def add_league_filter(df):
                  'Champion', 'Grand Champion', 'Royal Champion', 'Ultimate Champion']
     )
 
-def filter_dataframe(df, league_filter):
-    """Filter the DataFrame based on selected filters."""
+def filter_dataframe_league(df, league_filter):
+    """Filter the DataFrame based on selected league filter."""
     return df.filter(df["league"] == league_filter)
 
 def add_league_image(league):
@@ -56,11 +56,61 @@ def add_league_image(league):
         icon.resize((50, 50))
         
     st.sidebar.image(icon)
+
+
+def distinct_player_tags(filtered_df):
+    """Takes in the dataframe filtered by league.
+    Returns the distinct player tags in a list."""
+
+    # Creates a new dataframe with all player tags
+    all_tags = create_all_tags_df(filtered_df)
+
+    # Collects distinct player tags into a list
+    distinct_players = all_tags.select("player_tag").distinct()
+    distinct_players_list = distinct_players.select("player_tag").rdd.map(lambda x: x[0]).collect()
+    all_tags.unpersist()
     
+    return distinct_players_list
+
+# Join loser tags to bottom of winner tags column to create a column with all player tags
+def create_all_tags_df(filtered_df):
+    """Takes in filtered dataframe and combines winner tags and loser tags in one column.
+    Renames the new column to player_tags.
+    Returns a 1 column dataframe with all player tags."""
+    
+    winner_tags = filtered_df.select("winner_tag")
+    loser_tags = filtered_df.select("loser_tag")
+    all_tags = winner_tags.union(loser_tags)
+    all_tags.cache()
+    
+    # Rename column with tags to player_tag
+    all_tags = all_tags.withColumnRenamed("winner_tag", "player_tag")
+    return all_tags
+
+
+def add_player_filter(distinct_players):
+    """Add a filter for player tags."""
+    return st.sidebar.selectbox(
+        "Select Player Tag",
+        options=distinct_players
+    )
+
+
+def filter_dataframe_player(df, player_filter):
+    """Filter the DataFrame based on selected player filter.
+    Return filtered DataFrame"""
+    return df.filter((df.winner_tag == player_filter) | (df.loser_tag == player_filter))
+
+
 def apply_filter(df):
     """Apply all filters and return the filtered DataFrame."""
     st.sidebar.header("Filters")
     league_filter = add_league_filter(df)
     add_league_image(league_filter)
     
-    return filter_dataframe(df, league_filter)
+    filtered_df = filter_dataframe_league(df, league_filter)
+    
+    distinct_players = distinct_player_tags(filtered_df)
+    player_filter = add_player_filter(distinct_players)
+    
+    return filter_dataframe_player(filtered_df, player_filter)
